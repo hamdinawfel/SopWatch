1610f5e471a0f67f439183f0b05afd3cd6adbdd9
hamdinawfel <hamdii.nawfel@gmail.com>
2022-08-09 12:33:01 +0100

    resolve simple c# esercice form udemy course

---
+++ StopWatch/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new StopWatch();
+
+            for (int i = 0; i < 2; i++)
+            {
+                stopwatch.Start();
+
+                Thread.Sleep(1000);
+
+                stopwatch.Stop();
+
+                Console.WriteLine("Duration: " + stopwatch.GetInterval());
+                Console.WriteLine("Press Enter to run the StopWatch one more time");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        private bool _running = false;
+        public void Start()
+        {
+            if (_running)
+                throw new InvalidOperationException("StopWatch is already running!");
+
+            _startTime = DateTime.Now;
+            _running = true;
+
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+                throw new InvalidOperationException("StopWatch is not running!");
+
+            _endTime = DateTime.Now;
+            _running = false;
+
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _endTime;
+        }
+
+    }
+}
